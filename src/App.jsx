f433226444770e12a2a62212c27de91e67d8ced1
keingtobin <keingtobin@gmail.com>
2025-09-08 09:31:16 -0600

    working on basic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <p>Hello Portfolio!</p>
+          <Home></Home>
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import "../styles/home.css"
+
+import portfolioImage from "../assets/portfolio-img.jpg"
+export default function Home(){
+    return(
+        <>
+            <img src={portfolioImage}></img>
+        </>
+    )
+}
