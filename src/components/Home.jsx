cc0cd2bf8f62bade743ac40494132d7b429d0092
Keingtobin <keingtobin@gmail.com>
2025-09-08 10:03:27 -0600

    added basic info needed for site

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,12 @@ import "../styles/home.css"
 import portfolioImage from "../assets/portfolio-img.jpg"
 export default function Home(){
     return(
-        <>
+        <> 
             <img src={portfolioImage}></img>
+            <h1>Zach Huber</h1>
+            <p>I am a Senior at the University of Colorado Boulder earning a Bachelor's in Geography with a GIS Emphasis. I also have a Bachelor's in Computer Science with a Minor in Philosophy.</p>
+            <p>This is a portfolio made for GEOG4463, and it is where I will be sharing all of my work done for that class.</p>
+            <p>I will also be hosting any other projects I do for class or fun here. </p>
         </>
     )
 }
