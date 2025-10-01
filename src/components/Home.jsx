f3221d1b799f930d7ea6e2d2b25e44f75db2e927
Keingtobin <keingtobin@gmail.com>
2025-09-30 20:39:17 -0600

    added my climate project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import githubLogo from "../assets/github-logo.png"
 import linkedinLogo from "../assets/linkedin-logo.png"
 import mailLogo from "../assets/mail-logo.png"
 import woodyPreview from "../assets/woody.png"
+import cda from "../assets/cda.jpg"
 
 export default function Home(){
     function copyEMail(){
@@ -16,7 +17,9 @@ export default function Home(){
         <> 
         <div className="split-container">
             <div className="bio-card">
-                <img src={portfolioImage} className="headshot"></img>
+                <a href="/portfolio-site/">
+                    <img src={portfolioImage} className="headshot"></img>
+                </a>
                 <h1>Zach Huber</h1>
                 <div className="img-bar">
                     <a href="https://github.com/keingtobin" target="_blank">
@@ -37,16 +40,26 @@ export default function Home(){
             <div className="content-card">
                 <h1 align="center">Projects</h1>
                 <p align="center">Click on a project to learn more about it.</p>
-                <a href="/portfolio-site/woody" className="project-link">
-                    <img src={woodyPreview} alt="Woodland Park Project Preview"/>
-                </a>
+                <div className="project-links">
+                    <a href="/portfolio-site/woody" className="project-link">
+                        <h2>Woodland Park Dynamic Map</h2>
+                        <img src={woodyPreview} alt="Woodland Park Project Preview" height="200px" width="200px"/>
+                    </a>
+                    <a href="/portfolio-site/climate-proj" className="project-link">
+                        <h2>Coeur D'Alene Climate Project</h2>
+                        <img src={cda} alt="Climate Data Analysis Project Preview" height="200px" width="200px"/>
+                    </a>
+                    
+                </div>
+
+                
                 {/**<iframe className="woodyFrame" src="./woody.html" width="100%" height="500px" align="left"></iframe>**/}
             </div>
             : null}
-            {window.location.pathname === "/portfolio-site/woody" ? 
-            <p>Hello Woody Project!</p> : null}
             {window.location.pathname === "/portfolio-site/climate-proj" ? 
-            <p>Hello Climate Project!</p> : null}
+            <iframe src="./climate_notebook.html" height="100%" width="100%"></iframe> : null}
+            {window.location.pathname === "/portfolio-site/woody" ? 
+            <p>Need to add the information for woodland park here!</p> : null}
         </div>
         </>
     )
